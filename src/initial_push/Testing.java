bf8fa36a164270c8be1c15070f0ed2d9faaf65fd
bassvsguitar <oliver.zientarski@gmail.com>
2014-06-26 11:18:51 +0200

    commit from Thinkpad x220t

--- src/initial_push/Testing.java
+++ src/initial_push/Testing.java
@@ -5,8 +5,10 @@ public class Testing {
 	public static void main(String[] args) {
 		
 		System.out.println("This is the initial push to test my GitHub repository");
-		System.out.println("Initalizing initial pull...");
+		System.out.println("Initalizing initial push...");
 		System.out.println("Initial push successful!");
+		System.out.println("Initializing initall pull...");
+		System.out.println("Initial pull successful!");
 
 	}
 
