75149da622ad6b75a317c3212b31f91ef03cee02
bassvsguitar <oliver.zientarski@gmail.com>
2014-06-26 11:09:29 +0200

    second commit

--- src/initial_push/Testing.java
+++ src/initial_push/Testing.java
@@ -5,6 +5,8 @@ public class Testing {
 	public static void main(String[] args) {
 		
 		System.out.println("This is the initial push to test my GitHub repository");
+		System.out.println("Initalizing initial pull...");
+		System.out.println("Initial push successful!");
 
 	}
 
