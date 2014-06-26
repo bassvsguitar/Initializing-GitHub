c1d0bebb0aca785ed36435bb9e6dd96677b02903
bassvsguitar <oliver.zientarski@gmail.com>
2014-06-26 10:59:39 +0200

    initial commit

---
+++ src/initial_push/Testing.java
@@ -0,0 +1,11 @@
+package initial_push;
+
+public class Testing {
+
+	public static void main(String[] args) {
+		
+		System.out.println("This is the initial push to test my GitHub repository");
+
+	}
+
+}
