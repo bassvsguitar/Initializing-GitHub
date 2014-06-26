4eb1b3da8e11ce8749c45e6713c554ce05c70060
bassvsguitar <oliver.zientarski@gmail.com>
2014-06-26 15:16:25 +0200

    third commit from Thinkpad x220t

--- src/hello_git_hub/Testing.java
+++ src/hello_git_hub/Testing.java
@@ -5,6 +5,11 @@ public class Testing {
 	public static void main(String[] args) {
 		
 		System.out.println("Hi, I'm trying to get this whole GitHub thingy running!");
+		System.out.print("Note to myself:");
+		System.out.print("\n1.) Commit changes");
+		System.out.print("\n2.) Push to Upstream to get your repository updated");
+		System.out.print("\n3.) Fetch from Upstream");
+		System.out.print("\n4.) Send pull request to repository to get your local workspace synchronized");
 
 	}
 
